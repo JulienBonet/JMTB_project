abef8c051d895646227312979689e44ea6b3a220
JULIEN BONET <julienbonetpro@gmail.com>
2024-03-06 19:23:11 +0100

    Modular director

--- frontend/src/pages/MovieArtist/MovieDirectors.jsx
+++ frontend/src/pages/MovieArtist/MovieDirectors.jsx
@@ -1,18 +1,9 @@
 import { useState, useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+import { createTheme } from "@mui/material/styles";
 import "./movieArtist.css";
-import "../../assets/css/common_elements.css";
-import "../../assets/css/scrollButton.css";
-import MovieCount from "../../components/MovieCount/MovieCount";
-import AlphabetDropdown from "../../components/AlphabetOption/AlphabetDropdown";
-import Counter from "../../components/Counters/Counters";
-import AlphabeticBtn from "../../components/AlphabeticBtn/AlphabeticBtn";
-import ChronologicBtn from "../../components/ChronologicBtn/ChronologicBtn";
-import MovieThumbnail from "../../components/MovieThumbnail/MovieThumbnail";
-import DirectorBear from "../../assets/ico/director_bear_01.jpeg";
+import ArtistList from "../../components/ArtistList/ArtistList";
+import ArtistFilmo from "../../components/ArtistFilmo/ArtistFilmo";
 
 function MovieDirectors() {
   // DATAS
@@ -190,13 +181,15 @@ function MovieDirectors() {
         contrastText: "#242105",
       },
       artists_list: {
-        main: "#fefee2", // Assurez-vous que la couleur principale est correctement définie
+        main: "#fefee2",
         light: "#ffa500",
         dark: "#e59100",
         contrastText: "#242105",
       },
     },
   });
+
+  const origin = "directors";
 
   return (
     <main>
@@ -215,102 +208,29 @@ function MovieDirectors() {
         <div className="dashed_secondary_bar" />
         <section>
           <section className="artists_seach_container">
-            <section className="artists_groups">
-              <AlphabetDropdown onLetterChange={handleLetterChange} />
-              {search === "" && (
-                <div className="artists_groups_content">
-                  <ThemeProvider theme={theme}>
-                    <Stack spacing={2} direction="row" className="artists_list">
-                      {selectedDirectorByLetter.map((director) => (
-                        <Button
-                          key={director.id}
-                          variant="text"
-                          color="artists_list"
-                          size="small"
-                          className="artists_button"
-                          onClick={() => handleDirectorClick(director)}
-                        >
-                          {director.name}
-                        </Button>
-                      ))}
-                    </Stack>
-                  </ThemeProvider>
-                </div>
-              )}
-              {search !== "" && (
-                <div className="artists_groups_content">
-                  <ThemeProvider theme={theme}>
-                    <Stack spacing={2} direction="row" className="artists_list">
-                      {filteredDirectors.map((director) => (
-                        <Button
-                          key={director.id}
-                          variant="text"
-                          color="primary"
-                          size="small"
-                          className="artists_button"
-                          onClick={() => handleDirectorClick(director)}
-                        >
-                          {director.name}
-                        </Button>
-                      ))}
-                    </Stack>
-                  </ThemeProvider>
-                </div>
-              )}
-              {search === "" && (
-                <Counter origin="directors" countAmount={directorsAmount} />
-              )}
-              {search !== "" && (
-                <Counter
-                  origin="directors"
-                  countAmount={selectedDirectorAmount}
-                />
-              )}
-            </section>
-            <section className="filmo_artists">
-              {selectedDirector === "" && (
-                <section className="artists_bear">
-                  <section className="artists_bear_position">
-                    <div className="artists_bear_container">
-                      <div className="artists_pitch_container">
-                        <p className="artists_pitch">
-                          QUEL REALISATEUR CHERCHONS NOUS ?
-                        </p>
-                      </div>
-                      <img
-                        src={DirectorBear}
-                        alt="a Bear director"
-                        className="artists_bear_illustr"
-                      />
-                    </div>
-                  </section>
-                </section>
-              )}
-              {selectedDirector !== "" && (
-                <section className="artists_filmo">
-                  <div className="scroll_zone scroll_zone_2">
-                    <div className="artists_filmo_thumbs">
-                      {data.map((filmo) => (
-                        <MovieThumbnail key={filmo.id} data={filmo} />
-                      ))}
-                    </div>
-                  </div>
-                </section>
-              )}
-              <div className="btn_sort_container_search">
-                <AlphabeticBtn
-                  origin="artists"
-                  onClick={sortOrderA === "asc" ? movieSortedZ : movieSortedA}
-                />
-                <MovieCount movieAmount={movieAmount} />
-                <ChronologicBtn
-                  origin="artists"
-                  onClick={
-                    sortOrderY === "asc" ? movieSortedYearDesc : movieSortedYear
-                  }
-                />
-              </div>
-            </section>
+            <ArtistList
+              handleLetterChange={handleLetterChange}
+              search={search}
+              theme={theme}
+              selectedByLetter={selectedDirectorByLetter}
+              filteredArtist={filteredDirectors}
+              handleArtistClick={handleDirectorClick}
+              origin={origin}
+              artistAmount={directorsAmount}
+              selectedArtistAmount={selectedDirectorAmount}
+            />
+            <ArtistFilmo
+              selectedArtist={selectedDirector}
+              origin={origin}
+              data={data}
+              sortOrderA={sortOrderA}
+              movieSortedZ={movieSortedZ}
+              movieSortedA={movieSortedA}
+              sortOrderY={sortOrderY}
+              movieSortedYearDesc={movieSortedYearDesc}
+              movieSortedYear={movieSortedYear}
+              movieAmount={movieAmount}
+            />
           </section>
         </section>
       </section>
