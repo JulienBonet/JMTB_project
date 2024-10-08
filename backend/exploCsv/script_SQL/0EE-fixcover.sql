48ca3e62e79ba1b4b174a2cffeaaaabb68d45e61
JULIEN BONET <julienbonetpro@gmail.com>
2024-10-08 19:28:18 +0200

    resolve conflict in sql script

--- backend/exploCsv/script_SQL/0EE-fixcover.sql
+++ backend/exploCsv/script_SQL/0EE-fixcover.sql
@@ -1,14 +1,3 @@
-use jmdb2;
-
-UPDATE movies
-SET cover = '00_cover_default.jpg'
-WHERE id = 3614;
-
-UPDATE movies
-SET cover = '00_cover_default.jpg'
-WHERE id = 3521;
-
-SELECT * FROM movies;
 use jmdb;
 
 UPDATE movies
