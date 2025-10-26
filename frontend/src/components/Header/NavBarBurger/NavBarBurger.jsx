66140857dc0c7c8d13980f0d02042ef5a12e2819
JULIEN BONET <julienbonetpro@gmail.com>
2025-10-26 15:30:30 +0100

    modify burger menu

--- frontend/src/components/Header/NavBarBurger/NavBarBurger.jsx
+++ frontend/src/components/Header/NavBarBurger/NavBarBurger.jsx
@@ -1,49 +1,46 @@
 import * as React from "react";
-import { Link } from "react-router-dom"; // Pour la navigation
+import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import Divider from "@mui/material/Divider";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import "../../../assets/css/var_font_color.css";
 
 export default function BasicMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
 
   return (
     <div>
       <Button
-        id="bugerMenuButton"
+        id="burgerMenuButton"
         aria-controls={open ? "burgerMenu" : undefined}
         aria-haspopup="true"
         aria-expanded={open ? "true" : undefined}
         onClick={handleClick}
+        aria-label={open ? "Fermer le menu" : "Ouvrir le menu"}
       >
-        {/* Icônes avec stylisation */}
         {open ? (
-          <CloseIcon sx={{ fontSize: 30, color: "var(--color-01)" }} /> // Croix stylisée
+          <CloseIcon sx={{ fontSize: 30, color: "var(--color-01)" }} />
         ) : (
-          <MenuIcon sx={{ fontSize: 30, color: "var(--color-01)" }} /> // Burger stylisé
+          <MenuIcon sx={{ fontSize: 30, color: "var(--color-01)" }} />
         )}
       </Button>
 
-      {/* Menu stylisé */}
       <Menu
         id="burgerMenu"
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
         MenuListProps={{
-          "aria-labelledby": "bugerMenuButton",
+          "aria-labelledby": "burgerMenuButton",
         }}
         slotProps={{
           paper: {
@@ -53,32 +50,93 @@ export default function BasicMenu() {
               boxShadow: "0px 4px 12px rgba(0,0,0,0.1)",
               border: "1px solid var(--color-02)",
               borderRadius: "8px",
+              minWidth: 240,
+              paddingY: 1,
             },
           },
         }}
       >
-        {/* Navigation organisée */}
-        <MenuItem
-          onClick={handleClose}
-          component={Link}
-          to="/"
-          sx={{ width: "100%", justifyContent: "center" }}
-        >
-          HOME
-        </MenuItem>
+        {/* RECHERCHE GÉNÉRALE */}
         <MenuItem
           onClick={handleClose}
           component={Link}
           to="/movie_search"
-          sx={{ width: "100%", justifyContent: "center" }}
+          sx={{ justifyContent: "center" }}
         >
-          RECHERCHE
+          RECHERCHE FILMS
         </MenuItem>
+
+        <Divider sx={{ backgroundColor: "var(--color-02)", marginY: 1 }} />
+
+        {/* TITRE : RECHERCHE PAR (non cliquable) */}
+        <Box sx={{ px: 2, textAlign: "center" }}>
+          <Typography
+            sx={{
+              fontWeight: 700,
+              color: "var(--color-02)",
+              letterSpacing: 0.6,
+              marginBottom: 1,
+            }}
+            aria-hidden="true"
+          >
+            - RECHERCHE PAR -
+          </Typography>
+        </Box>
+
+        {/* LISTE DES RECHERCHES PAR */}
+        <MenuItem
+          onClick={handleClose}
+          component={Link}
+          to="/movie_directors"
+          sx={{ justifyContent: "center" }}
+        >
+          RÉALISATEUR
+        </MenuItem>
+
+        <MenuItem
+          onClick={handleClose}
+          component={Link}
+          to="/movie_casting"
+          sx={{ justifyContent: "center" }}
+        >
+          CASTING
+        </MenuItem>
+
+        <MenuItem
+          onClick={handleClose}
+          component={Link}
+          to="/movie_screenwriters"
+          sx={{ justifyContent: "center" }}
+        >
+          SCÉNARISTE
+        </MenuItem>
+
+        <MenuItem
+          onClick={handleClose}
+          component={Link}
+          to="/movie_music"
+          sx={{ justifyContent: "center" }}
+        >
+          COMPOSITEUR
+        </MenuItem>
+
+        <MenuItem
+          onClick={handleClose}
+          component={Link}
+          to="/movie_studio"
+          sx={{ justifyContent: "center" }}
+        >
+          STUDIO
+        </MenuItem>
+
+        <Divider sx={{ backgroundColor: "var(--color-02)", marginY: 1 }} />
+
+        {/* ADMIN */}
         <MenuItem
           onClick={handleClose}
           component={Link}
           to="/admin_feat"
-          sx={{ width: "100%", justifyContent: "center" }}
+          sx={{ justifyContent: "center" }}
         >
           ADMIN
         </MenuItem>
